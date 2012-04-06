d2146d2cde7f1183bc4e69f55740a70d6f6112d9
iamrohitbanga@gmail.com <iamrohitbanga@gmail.com@36d76967-4552-1772-9c8b-f78efaaca024>
2012-04-06 21:57:09 +0000

    Ponte Croft Partial Implementation

--- trunk/src/srmdata/SRMTester.java
+++ trunk/src/srmdata/SRMTester.java
@@ -4,7 +4,11 @@ public class SRMTester {
 
 	
 	public static void main(String[] args) throws Exception {
-		NSDLIndex.createIndex();
+//		NSDLIndex.createIndex();
 //		NSDLIndex.computeStatistics();
+		
+		StructuredRelevanceModel srm;
+		srm = new StructuredRelevanceModel();
+		srm.generateTestTrainSets_1();
 	}
 }
