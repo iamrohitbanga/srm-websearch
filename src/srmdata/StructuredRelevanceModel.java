844f22a4515092bad4d817fd832c63abed10d764
iamrohitbanga@gmail.com <iamrohitbanga@gmail.com@36d76967-4552-1772-9c8b-f78efaaca024>
2012-04-07 20:09:36 +0000

    Ponte Croft faster

--- src/srmdata/StructuredRelevanceModel.java
+++ src/srmdata/StructuredRelevanceModel.java
@@ -1,12 +1,10 @@
 package srmdata;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
@@ -27,54 +25,12 @@ public class StructuredRelevanceModel {
 	Set<Integer> testDocIds;
 	Set<Integer> trainDocIds;
 
-	class TermDocPair {
-		String term;
-		int docID;
-
-		public TermDocPair(String t, int d) {
-			term = t;
-			docID = d;
-		}
-
-		@Override
-		public boolean equals(Object a) {
-			TermDocPair td1 = (TermDocPair) a;
-			TermDocPair td2 = (TermDocPair) this;
-			return (td1.term.equals(td2.term) && td1.docID == td2.docID);
-		}
-	};
-
 	double[] modelScores;
 	
-	class DocPair {
-		int d1;
-		int d2;
-		public DocPair(int doc1, int doc2) {
-			d1 = doc1;
-			d2 = doc2;
-		}
-		
-		@Override
-		public int hashCode() {
-			int hash = 3571;
-			hash = hash * 31 + d1;
-			hash = hash * 31 + d2;
-			return hash;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			return (d1 == ((DocPair) o).d1 && d2 == ((DocPair) o).d2);
-		}
-	};
-
-	Map<DocPair, Double> model;
-
 	public StructuredRelevanceModel() {
 		allDocIds = new HashSet<Integer>();
 		testDocIds = new HashSet<Integer>();
 		trainDocIds = new HashSet<Integer>();
-		model = new TreeMap<DocPair, Double>();
 	}
 
 	void generateTestTrainSets_1() throws Exception {
@@ -121,7 +77,18 @@ public class StructuredRelevanceModel {
 		File nsdl_index_dir = new File(NSDLIndex.NSDL_INDEX_DIR_NAME);
 		IndexReader ir = IndexReader.open(FSDirectory.open(nsdl_index_dir), true);
 
-		computePriors(ir, testDocIds, trainDocIds, "title");
+		long t1 = System.nanoTime();
+			computePriors(ir, testDocIds, trainDocIds, "title");
+		long t2 = System.nanoTime();
+		System.out.println("                Time Taken: " + ((double)(t2-t1)) / 1E9);
+
+			computePriors(ir, testDocIds, trainDocIds, "desc");
+		long t3 = System.nanoTime();
+		System.out.println("                Time Taken: " + ((double)(t3-t2)) / 1E9);
+
+			computePriors(ir, testDocIds, trainDocIds, "content");
+		long t4 = System.nanoTime();
+		System.out.println("                Time Taken: " + ((double)(t4-t3)) / 1E9);
 
 		ir.close();
 	}
@@ -138,14 +105,30 @@ public class StructuredRelevanceModel {
 	void computePriors(IndexReader ir, Set<Integer> testDocs, Set<Integer> modelDocIds, String fieldName) throws Exception {
 
 		Map<Integer, Integer> doc_length = new HashMap<Integer, Integer>();
-		for (Integer docID : trainDocIds) {
+		for (Integer docID : modelDocIds) {
 			TermFreqVector tfVec = ir.getTermFreqVector(docID, "title");
 			doc_length.put(docID, tfVec.size());
 		}
 
-		modelScores = new double[modelDocIds.size() * testDocIds.size()];
+		modelScores = new double[modelDocIds.size() * testDocs.size()];
 		for (int i = 0; i < modelScores.length; ++i) {
 			modelScores[i] = 1.0;
+		}
+
+		int minMD = Integer.MAX_VALUE;
+		int maxMD = Integer.MIN_VALUE;
+		int mdSize = modelDocIds.size();
+		for (int md : modelDocIds) {
+			minMD = (md < minMD) ? md : minMD;
+			maxMD = (md > minMD) ? md : maxMD;
+		}
+		
+		int minQ = Integer.MAX_VALUE;
+		int maxQ = Integer.MIN_VALUE;
+		int qSize = testDocs.size();
+		for (int q : testDocs) {
+			minQ = (q < minQ) ? q : minQ;
+			maxQ = (q > maxQ) ? q : maxQ;
 		}
 		
 		int collectionSize = 0;
@@ -159,7 +142,9 @@ public class StructuredRelevanceModel {
 		terms.close();
 
 		System.out.println("Collection Size: " + collectionSize);
+		int[] docsForTerm = new int[testDocs.size()];
 		
+//		int termID = 0;
 		terms = ir.terms();
 		while (terms.next()) {
 
@@ -167,36 +152,40 @@ public class StructuredRelevanceModel {
 			if (!t.field().equals(fieldName) || containsNumber(t.text()))
 				continue;
 
-//			System.out.println("term: " + t.text());
+//			System.out.println("termID: " + termID + "  term: " + t.text());
+//			termID++;
+
+			for (int i = 0; i < docsForTerm.length; ++i) {
+				docsForTerm[i] = 1;
+			}
 			TermDocs termDocs = ir.termDocs(t);
-			Set<Integer> docsForTerm = new HashSet<Integer>();
-			while (termDocs.next())
-				docsForTerm.add(termDocs.doc());
+			while (termDocs.next()) {
+				if (!testDocs.contains(termDocs.doc()))
+					continue;
+				docsForTerm[termDocs.doc()-minQ] = 0;
+			}
 			termDocs.close();
 
-			long t1 = System.nanoTime();
+//			long t1 = System.nanoTime();
+
 			Map<Integer, Double> mle = compute_mlestimate(ir, fieldName, modelDocIds, t, doc_length, collectionSize);
-			DocPair dp = new DocPair(1, 1);
-			long t2 = System.nanoTime();
-			System.out.println("                Time Taken: " + ((double)(t2-t1)) / 1E9);
-			for (Integer md : modelDocIds) {
-				double val = mle.get(md);
-				dp.d2 = md;
-				for (Integer q : testDocs) {
-					dp.d1 = q;
-					Double prob = model.get(dp);
-					if (prob == null) {
-						prob = 1.0;
-						model.put(dp, prob);
-					}
-					if (!docsForTerm.contains(q))
-						val = 1.0 - val;
-					prob = prob * val;
-					model.put(dp, prob);
+
+//			long t2 = System.nanoTime();
+//			System.out.println("                Time Taken: " + ((double)(t2-t1)) / 1E9);
+
+			double[] val = new double[2];
+			for (int md=0; md < mdSize; ++md) {
+
+				val[0] = mle.get(md+minMD);
+				val[1] = 1.0 - val[0];
+
+				for (int q = 0; q < qSize; ++q) {
+					modelScores[md*qSize + q] += val[docsForTerm[q]];
 				}
 			}
-			long t3 = System.nanoTime();
-			System.out.println("Time Taken: " + ((double)(t3-t2)) / 1E9);
+
+//			long t3 = System.nanoTime();
+//			System.out.println("Time Taken: " + ((double)(t3-t2)) / 1E9);
 		}
 		terms.close();
 	}
@@ -210,13 +199,11 @@ public class StructuredRelevanceModel {
 		compute_avgs(ir, fieldName, modelDocIds, t, pavg, meanfreq, collectionFreq);
 
 		Map<Integer,Double> mlEstimates = new HashMap<Integer, Double>();
-//		long t1 = System.nanoTime();
 
 		TermDocs termDocs = ir.termDocs(t);
 		while (termDocs.next()) {
 
 			int d = termDocs.doc();
-
 			if (!modelDocIds.contains(d))
 				continue;
 
@@ -233,9 +220,6 @@ public class StructuredRelevanceModel {
 			if (!mlEstimates.containsKey(md))
 				mlEstimates.put(md, defaultVal);
 		}
-		
-//		long t2 = System.nanoTime();
-//		System.out.println("Time taken: " + ((double)(t2-t1)) / 1E9 );
 
 		return mlEstimates;
 	}
