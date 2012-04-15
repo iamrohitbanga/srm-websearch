a0a1ca93622ceb794dbc70be018fb6a6d6556322
hmehrotra.86@gmail.com <hmehrotra.86@gmail.com@36d76967-4552-1772-9c8b-f78efaaca024>
2012-04-15 21:48:10 +0000

    SRM

--- trunk/src/srmdata/SRM.java
+++ trunk/src/srmdata/SRM.java
@@ -2,14 +2,21 @@ package srmdata;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.snowball.SnowballAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -18,32 +25,37 @@ import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 
 import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Ordering;
 
 /*
+ * Analyze text and add to stop word list particularly special characters - remove numbers
  * To change - Ease constraints to include records with multiple audience, education level and subject in train/test data sets
- * Shuffle records into training and test sets randomly
- * Add education level for prediction
- * While creating indexes - choose the number of words to allow - stemming
- * Some basic NLP like stemming etc, all elements in lower case
+ * Shuffle records into training and test sets randomly - include records with uniformly distributed audience, edu level and subject
+ * improve tuning parameters - use term frequency in smoothing
+ * Split by delimiters
  */
 
+@SuppressWarnings("deprecation")
 public class SRM 
 {
 	List<Integer> allDocIds;
 	List<Integer> testDocIds;
 	List<Integer> trainDocIds;
 	
-	float titleWeight = 0.9f;
-	float contentWeight = 0.6f;
-	float descWeight = 0.7f;
+	float titleWeight = 100f;
+	float contentWeight = 0.01f;
+	float descWeight = 0.01f;
 		
 	Map <Integer , Float> contentScore;
 	Map <Integer , Float> descriptionScore;
 	Map <Integer , Float> titleScore;
+	
+	public static Set<String> stopWords;
+	public static Version VERSION = Version.LUCENE_CURRENT;
 	
 	public SRM() 
 	{
@@ -55,55 +67,86 @@ public class SRM
 	public float calculateDeltaKernelScore(Integer testDocId)
 	{
 		float score = 0.0f;
-		String rContent , rTitle, rDesc , wContent , wTitle , wDesc;
+		Set <String> rContent = new HashSet<String>();
+		Set <String> rTitle = new HashSet<String>();
+		Set <String> rDesc = new HashSet<String>();
+	
 		Map <String , Integer> titleMap;
 		Map <String , Integer> descMap;
 		Map <String , Integer> contentMap;
-		StringTokenizer sTokenizer;
+		
 		String tempToken;
+		
 		int titleNI, contentNI, descNI;
 		double titleSimScore , descSimScore, contentSimScore, simScore;
 		
 		Map <String , Double> audienceVocabularyScore = new HashMap<String , Double>();
 		Map <String , Double> subjectVocabularyScore = new HashMap<String , Double>();
 		Map <String , Double> educationVocabularyScore = new HashMap<String , Double>();
-				
+		
+		String []predictedAudience;
+		String []predictedSubject;
+		StringTokenizer subjectVocabTokens , audienceVocabTokens;
+		Set <String> subjectVocabSet , audienceVocabSet;
+		
+		Iterator <String> subjectVocabSetIterator , audienceVocabSetIterator;
+		int i , length;
+		
 		Document doc;
+		Iterator<String> iterator;
 		
 		File nsdl_index_dir = new File(NSDLIndex.NSDL_INDEX_DIR_NAME);
 		IndexReader ir;
 		try 
 		{
+			System.out.println("Test doc ID : " + testDocId);
 			ir = IndexReader.open(FSDirectory.open(nsdl_index_dir), true);
 			//IndexSearcher searcher = new IndexSearcher(ir);
+			SnowballAnalyzer analyzer = new SnowballAnalyzer(SRM.VERSION , "Porter" , SRM.stopWords);
+			TokenStream ts ;
+			TermAttribute termAtt;
 			
 			// Query data
 			doc = (Document)ir.document(testDocId);
-			rTitle = doc.get("title");
-			rContent = doc.get("content");
-			rDesc = doc.get("desc");
+			ts = analyzer.tokenStream("title", new StringReader(doc.get("title")));
+			termAtt = ts.addAttribute(TermAttribute.class);	
+			while (ts.incrementToken())
+			{
+				rTitle.add(termAtt.term());
+			}
 			
-			System.out.println("Query audience : " + doc.get("audience"));
-			System.out.println("Quer subject : " + doc.get("subject"));
+			ts = analyzer.tokenStream("content", new StringReader(doc.get("content")));
+			termAtt = ts.addAttribute(TermAttribute.class);	
+			while (ts.incrementToken())
+			{
+				rContent.add(termAtt.term());
+			}
 			
+			ts = analyzer.tokenStream("desc", new StringReader(doc.get("desc")));
+			termAtt = ts.addAttribute(TermAttribute.class);	
+			while (ts.incrementToken())
+			{
+				rDesc.add(termAtt.term());
+			}
+						
 			// Iterate over all training records to find the score of train , test document pair
 			Iterator <Integer> trainIterator = trainDocIds.iterator();
 			while (trainIterator.hasNext())
-			{
+			{ 
 				doc = (Document)ir.document((Integer)trainIterator.next());
-				
-				/********************** Title Similarity Score ****************/
-				wTitle = doc.get("title");
-				sTokenizer = new StringTokenizer(wTitle);
 								
+				//********************** Title Similarity Score ***************
+				ts = analyzer.tokenStream("title", new StringReader(doc.get("title")));
+				termAtt = ts.addAttribute(TermAttribute.class);		
+				
 				// Construct a HashMap of Train record title
 				titleMap = new HashMap<String , Integer>();
 				titleNI = 0;
 				
-				while(sTokenizer.hasMoreTokens())
+				while(ts.incrementToken())
 				{
 					titleNI++;
-					tempToken = sTokenizer.nextToken();
+					tempToken = termAtt.term();
 					
 					if (titleMap.containsKey(tempToken))
 					{
@@ -113,13 +156,13 @@ public class SRM
 						titleMap.put(tempToken, 1);
 				}
 				
-				//Tokenize the query title and find similarity score based on titles
-				sTokenizer = new StringTokenizer(rTitle);
+				// Iterate over query title set to find similarity score
+				iterator = rTitle.iterator();
 				titleSimScore = 0.0;
 				
-				while(sTokenizer.hasMoreTokens())
+				while(iterator.hasNext())
 				{
-					tempToken = sTokenizer.nextToken();
+					tempToken = iterator.next();
 					if (titleMap.containsKey(tempToken))
 					{
 						titleSimScore += ((double)titleMap.get(tempToken) + 1)/(titleNI + 2);
@@ -130,18 +173,18 @@ public class SRM
 					}
 				}
 				
-				/********************** Description Similarity Score ****************/
-				wDesc = doc.get("desc");
-				sTokenizer = new StringTokenizer(wDesc);
+				//********************** Description Similarity Score ****************
+				ts = analyzer.tokenStream("desc", new StringReader(doc.get("desc")));
+				termAtt = ts.addAttribute(TermAttribute.class);	
 								
-				// Construct a HashMap of Train record desc
+				// Construct a HashMap of Train record description
 				descMap = new HashMap<String , Integer>();
 				descNI = 0;
 				
-				while(sTokenizer.hasMoreTokens())
+				while(ts.incrementToken())
 				{
 					descNI++;
-					tempToken = sTokenizer.nextToken();
+					tempToken = termAtt.term();
 					
 					if (descMap.containsKey(tempToken))
 					{
@@ -150,14 +193,14 @@ public class SRM
 					else
 						descMap.put(tempToken, 1);
 				}
+								
+				/// Iterate over query description set to find similarity score
+				iterator = rDesc.iterator();
+				descSimScore = 0.0;
 				
-				//Tokenize the query description and find similarity score based on description
-				sTokenizer = new StringTokenizer(rDesc);
-				descSimScore = 0.f;
-				
-				while(sTokenizer.hasMoreTokens())
+				while(iterator.hasNext())
 				{
-					tempToken = sTokenizer.nextToken();
+					tempToken = iterator.next();
 					if (descMap.containsKey(tempToken))
 					{
 						descSimScore += ((double)descMap.get(tempToken) + 1)/(descNI + 2);
@@ -168,18 +211,18 @@ public class SRM
 					}
 				}
 				
-				/********************** Content Similarity Score ****************/
-				wContent = doc.get("content");
-				sTokenizer = new StringTokenizer(wContent);
+				//********************** Content Similarity Score ****************
+				ts = analyzer.tokenStream("content", new StringReader(doc.get("content")));
+				termAtt = ts.addAttribute(TermAttribute.class);	
 								
 				// Construct a HashMap of Train record content
 				contentMap = new HashMap<String , Integer>();
 				contentNI = 0;
 				
-				while(sTokenizer.hasMoreTokens())
+				while(ts.incrementToken())
 				{
 					contentNI++;
-					tempToken = sTokenizer.nextToken();
+					tempToken = termAtt.term();
 					
 					if (contentMap.containsKey(tempToken))
 					{
@@ -189,13 +232,13 @@ public class SRM
 						contentMap.put(tempToken, 1);
 				}
 				
-				// Tokenize the query content and find similarity score based on content
-				sTokenizer = new StringTokenizer(rContent);
-				contentSimScore = 0;
+				// Iterate over query content set to find similarity score
+				iterator = rContent.iterator();
+				contentSimScore = 0.0;
 				
-				while(sTokenizer.hasMoreTokens())
+				while(iterator.hasNext())
 				{
-					tempToken = sTokenizer.nextToken();
+					tempToken = iterator.next();
 					if (contentMap.containsKey(tempToken))
 					{
 						contentSimScore += ((double)contentMap.get(tempToken) + 1)/(contentNI + 2);
@@ -204,17 +247,31 @@ public class SRM
 					{
 						contentSimScore += 1.0 / (contentNI + 2);
 					}
+				}		
+				//****************** Computer overall similarity score - Cross entropy **********************
+				simScore = titleWeight * titleSimScore + contentWeight * contentSimScore + descWeight * descSimScore;
+								
+				// Update the audience vocabulary Score
+				predictedAudience = doc.getValues("audience");
+				length = predictedAudience.length;
+				i = 0;
+				
+				// Construct vocabulary set for this doc
+				audienceVocabSet = new HashSet<String>();
+				while(i < length)
+				{
+					audienceVocabTokens = new StringTokenizer(predictedAudience[i], "':/");
+					while(audienceVocabTokens.hasMoreTokens())
+					{
+						audienceVocabSet.add(audienceVocabTokens.nextToken().toLowerCase());
+					}
+					i++;
 				}
 				
-				/****************** Computer overall similarity score - Cross entropy **********************/
-				simScore = titleWeight * titleSimScore + contentWeight * contentSimScore + descWeight * descSimScore;
-				//System.out.println("Sim " + simScore);
-				
-				// Update the audience vocabulary Score
-				sTokenizer = new StringTokenizer(doc.get("audience"));
-				while(sTokenizer.hasMoreTokens())
+				audienceVocabSetIterator = audienceVocabSet.iterator();
+				while(audienceVocabSetIterator.hasNext())
 				{
-					tempToken = sTokenizer.nextToken();
+					tempToken = audienceVocabSetIterator.next();
 					if (audienceVocabularyScore.containsKey(tempToken))
 					{
 						audienceVocabularyScore.put(tempToken, audienceVocabularyScore.get(tempToken) + simScore);
@@ -224,10 +281,26 @@ public class SRM
 				}
 				
 				// Update the subject Vocabulary Score
-				sTokenizer = new StringTokenizer(doc.get("subject"));
-				while(sTokenizer.hasMoreTokens())
+				predictedSubject = doc.getValues("subject");
+				length = predictedSubject.length;
+				i = 0;
+				
+				// Construct vocabulary set for this doc
+				subjectVocabSet = new HashSet<String>();
+				while(i < length)
 				{
-					tempToken = sTokenizer.nextToken();
+					subjectVocabTokens = new StringTokenizer(predictedSubject[i], "':/");
+					while(subjectVocabTokens.hasMoreTokens())
+					{
+						subjectVocabSet.add(subjectVocabTokens.nextToken().toLowerCase());
+					}
+					i++;
+				}
+				
+				subjectVocabSetIterator = subjectVocabSet.iterator();
+				while(subjectVocabSetIterator.hasNext())
+				{
+					tempToken = subjectVocabSetIterator.next();
 					if (subjectVocabularyScore.containsKey(tempToken))
 					{
 						subjectVocabularyScore.put(tempToken, subjectVocabularyScore.get(tempToken) + simScore);
@@ -235,27 +308,23 @@ public class SRM
 					else
 						subjectVocabularyScore.put(tempToken , simScore);
 				}
-				
 			}
 			// Display the sorted audience vocabulary
-			Ordering valueComparator = Ordering.natural().onResultOf(Functions.forMap(audienceVocabularyScore)).compound(Ordering.natural());
+			/*Ordering valueComparator = Ordering.natural().onResultOf(Functions.forMap(audienceVocabularyScore)).compound(Ordering.natural());
 			ImmutableSortedMap audienceVocabSortedMap = ImmutableSortedMap.copyOf(audienceVocabularyScore, valueComparator);
-			System.out.println(audienceVocabSortedMap); 
+			System.out.println(audienceVocabSortedMap);*/ 
 			
-			/* // Display the sorted vocabulary
+			// Display the sorted vocabulary
 			Ordering valueComparator = Ordering.natural().onResultOf(Functions.forMap(subjectVocabularyScore)).compound(Ordering.natural());
 			ImmutableSortedMap subjectVocabSortedMap = ImmutableSortedMap.copyOf(subjectVocabularyScore, valueComparator);
-			System.out.println(subjectVocabSortedMap); */
+			System.out.println(subjectVocabSortedMap); 
 		} 
-		catch (CorruptIndexException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		catch (IOException e) {
+		catch (Exception e) 
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+		
 		return score;
 	}
 	
@@ -292,15 +361,22 @@ public class SRM
 
 		TopDocs t = searcher.search(nq, 20000);
 		ScoreDoc[] hits = t.scoreDocs;
+		
 		double testTrainRatio = 0.8;
 		int maxTrain = (int) (testTrainRatio * hits.length);
 		for (int i = 0; i < hits.length; ++i) 
 		{
 			allDocIds.add(hits[i].doc);
+		}
+		Collections.shuffle(allDocIds);
+		
+		Iterator iterator = allDocIds.iterator();
+		for (int i = 0; i < hits.length && iterator.hasNext(); ++i)
+		{
 			if (i < maxTrain)
-				trainDocIds.add(hits[i].doc);
+				trainDocIds.add((Integer)iterator.next());
 			else
-				testDocIds.add(hits[i].doc);
+				testDocIds.add((Integer)iterator.next());
 		}
 
 		System.out.println("Total Number of Documents: " + allDocIds.size());
@@ -311,22 +387,24 @@ public class SRM
 		ir.close();
 	}
 	
-	public static void main(String [] args)
+	public static void main(String [] args) 
 	{
 		SRM srmModel = new SRM();
 		int count = 0;
+				
 		try 
 		{
+			NSDLIndex.constructStopWordSet();
+			SRM.stopWords = NSDLIndex.stopWords;
+			
 			srmModel.generateTestTrainSets();
 			
 			// For each field - content, title, desc of all documents in testing set query the training set and find probability score of each documnet
 			Iterator <Integer> testIterator = srmModel.testDocIds.iterator();
-			
-			while (testIterator.hasNext() && count < 100)
+			while (testIterator.hasNext() && count < 5)
 			{
 				count++;
 				srmModel.calculateDeltaKernelScore((Integer)testIterator.next());
-				System.out.println(" Test Id:" + srmModel.testDocIds);
 			}
 		} 
 		catch (Exception e) 
