681f82154a93852edf2cde43f7ee6ac8ebd5d455
iamrohitbanga@gmail.com <iamrohitbanga@gmail.com@36d76967-4552-1772-9c8b-f78efaaca024>
2012-04-16 07:12:39 +0000

    Fixed few bugs ... results are better but accuracy is still very low.

--- trunk/src/srmdata/PriorCalculator.java
+++ trunk/src/srmdata/PriorCalculator.java
@@ -1,17 +1,12 @@
 package srmdata;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.TermDocs;
 import org.apache.lucene.index.TermEnum;
-import org.apache.lucene.index.TermFreqVector;
 
 public class PriorCalculator {
 
@@ -24,7 +19,7 @@ public class PriorCalculator {
 	IndexReader testIR;
 	String fieldName;
 	List<Term> allTerms;
-	Map<Integer,Integer> doc_length;
+	int[] doc_length;
 	double[][][] modelScores;
 
 	public PriorCalculator(IndexReader testIR, IndexReader trainIR, String fieldName) {
@@ -40,11 +35,11 @@ public class PriorCalculator {
 		nTestDocs = testIR.numDocs();
 
 		// find number of terms in all training documents for the given field
-		doc_length = new HashMap<Integer, Integer>();
+		doc_length = new int[nTrainDocs];
 		for (int docID = 0; docID < nTrainDocs; ++docID) {
 			if (trainIR.document(docID) == null)
 				continue;
-			doc_length.put(docID, getNumTermsInDocument(trainIR,docID,fieldName));
+			doc_length[docID] = StructuredRelevanceModel.getNumTermsInDocument(trainIR,docID,fieldName);
 		}
 
 		modelScores = new double[numThreads][nTrainDocs][nTestDocs];
@@ -54,23 +49,23 @@ public class PriorCalculator {
 					modelScores[threadNum][i][j] = 1.0;
 		}
 
-		collectionSize = findCollectionSize(trainIR, fieldName);
+		collectionSize = StructuredRelevanceModel.findCollectionSize(trainIR, fieldName);
 		allTerms = new ArrayList<Term>();
 
-		TermEnum terms = testIR.terms();
-		boolean trainDone = false;
+		TermEnum terms = trainIR.terms();
+//		boolean trainDone = false;
 		while (true) {
 			boolean hasNext = terms.next();
 			if (!hasNext) {
 				terms.close();
-				if (trainDone)
+//				if (trainDone)
 					break;
-				terms = trainIR.terms();
-				trainDone = true;
-				continue;
+//				terms = trainIR.terms();
+//				trainDone = true;
+//				continue;
 			}
 			Term t = terms.term();
-			if (!t.field().equals(fieldName) || containsNumber(t.text()))
+			if (!t.field().equals(fieldName) || StructuredRelevanceModel.containsNumber(t.text()))
 				continue;
 			allTerms.add(t);
 		}
@@ -113,122 +108,15 @@ public class PriorCalculator {
 			for (int i = 0; i < finalModelScores.length; ++i) {
 				for (int j = 0; j < finalModelScores[i].length; ++j) {
 					finalModelScores[i][j] *= modelScores[threadNum][i][j];
-					if (modelScores[threadNum][i][j] == 0.0) {
-						System.out.println("here " + threadNum + " " + i + " " + j);
-						System.exit(0);
-					}
+//					if (modelScores[threadNum][i][j] == 0.0) {
+//						System.out.println("here " + threadNum + " " + i + " " + j);
+//						System.exit(0);
+//					}
 				}
 			}
 		}
 		
 		return finalModelScores;
-	}
-
-	int getNumTermsInDocument(IndexReader ir, int docID, String fieldName) throws IOException {
-		TermFreqVector tfVec = ir.getTermFreqVector(docID, fieldName);
-		return ((tfVec == null) ? 0 : tfVec.size());
-	}
-
-	private Integer findCollectionSize(IndexReader ir, String fieldName) throws IOException {
-		int collectionSize = 0;
-		TermEnum terms = ir.terms();
-		while (terms.next()) {
-			Term t = terms.term();
-			if (!t.field().equals(fieldName) || containsNumber(t.text()))
-				continue;
-			TermDocs termDocs = ir.termDocs(t);
-			while (termDocs.next()) {
-				collectionSize += termDocs.freq();
-			}
-			termDocs.close();
-		}
-		terms.close();
-		return collectionSize;
-	}
-
-	boolean containsNumber(String str) {
-		for (int i = 0; i < str.length(); ++i) {
-			if (str.charAt(i) >= '0' && str.charAt(i) <= '9')
-				return true;
-		}
-		return false;
-	}
-
-	private double[] compute_mlestimate(IndexReader ir, String fieldName,
-			Term t, Map<Integer, Integer> doc_length, int collectionSize) throws Exception {
-
-		List<Double> avgs = compute_avgs(ir, fieldName, t);
-		Double pavg = avgs.get(0);
-		Double meanfreq = avgs.get(1);
-		Double collectionFreq = avgs.get(2);
-
-		if (collectionFreq == 0.0) {
-			return null;
-		}
-		
-//		Map<Integer,Double> mlEstimates = new HashMap<Integer, Double>();
-		double[] mlEstimates = new double[ir.maxDoc()];
-
-		double term1 = meanfreq / (1.0 + meanfreq);
-		double term2 = 1.0 / (1.0 + meanfreq);
-		TermDocs termDocs = ir.termDocs(t);
-		while (termDocs.next()) {
-			int d = termDocs.doc();
-			int tf = termDocs.freq();
-			double R = term2 * Math.pow(term1,tf);
-			double pml = ((double)tf) / doc_length.get(d);
-			double val = Math.pow(pml, 1.0-R) * Math.pow(pavg, R);
-			mlEstimates[d] = val;
-		}
-		termDocs.close();
-
-		double defaultVal = ((double)collectionFreq) / collectionSize;
-		for (int md = 0; md < ir.maxDoc(); ++md) {
-			if (mlEstimates[md] == 0.0)
-				mlEstimates[md] = defaultVal;
-		}
-
-		return mlEstimates;
-	}
-
-	private List<Double> compute_avgs(IndexReader ir, String fieldName,
-			Term t) throws Exception {
-
-		double collectionFreq = 0;
-		double pavg = 0.0;
-		double meanfreq = 0.0;
-
-		int count = 0;
-		TermDocs termDocs = ir.termDocs(t);
-		while (termDocs.next()) {
-
-			int d = termDocs.doc();
-			int tf = termDocs.freq();
-			TermFreqVector tfv = ir.getTermFreqVector(d, fieldName);
-			int dl = tfv.size();
-			if (dl == 0)
-			{
-				System.out.println("d:" + d + " dl:" + dl);
-				System.exit(0);
-			}
-			double pml = ((double)tf) / dl;
-			pavg = pavg + pml;
-			meanfreq = meanfreq + tf;
-			collectionFreq = collectionFreq + tf;
-			count++;
-		}
-		termDocs.close();
-
-		if (count == 0) {
-			pavg = 0.0;
-			meanfreq = 0.0;
-		}
-		else {
-			pavg = pavg / count;
-			meanfreq = meanfreq / count;
-		}
-		return 
-			Arrays.asList(pavg, meanfreq, collectionFreq);
 	}
 
 	class PriorCalculatorRunnable implements Runnable {
@@ -250,9 +138,10 @@ public class PriorCalculator {
 		public void run() {
 			try {
 				double score[] = new double[2];
+				double[] mle = new double[nTrainDocs];
 				for (int index = fromIndex; index <= toIndex; ++index) {
 					Term t = allTerms.get(index);
-					double[] mle = compute_mlestimate(trainIR, fieldName, t, doc_length, collectionSize);
+					StructuredRelevanceModel.compute_mlestimate(trainIR, fieldName, t, doc_length, collectionSize, mle);
 					if (mle == null)
 						continue;
 	
